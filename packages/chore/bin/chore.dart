d3f55f7df47be991a78c62152d6551a7efc41819
Matan Lurey <matan@lurey.org>
2024-07-21 19:00:07 -0700

    Move from a tool model to a binary model.

---
+++ packages/chore/bin/chore.dart
@@ -0,0 +1,7 @@
+#!/usr/bin/env dart
+
+import 'package:chore/chore.dart';
+
+void main(List<String> args) async {
+  await Runner().run(args);
+}
---
+++ packages/chore/test/example_test.dart
@@ -0,0 +1,9 @@
+// ignore: unused_import
+import 'package:chore/chore.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('example', () {
+    expect(1 + 1, 2);
+  });
+}
