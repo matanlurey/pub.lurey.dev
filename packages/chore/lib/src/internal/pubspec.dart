2a5b1379a4b9ce5e3d792bb21405d06d2f8a76aa
Matan Lurey <matan@lurey.org>
2024-10-17 11:54:14 -0700

    ++

--- packages/chore/lib/src/internal/pubspec.dart
+++ packages/chore/lib/src/internal/pubspec.dart
@@ -1,13 +1,15 @@
 @internal
 library;
 
+import 'package:chore/src/internal/yaml_wrapper.dart';
 import 'package:meta/meta.dart';
 import 'package:yaml/yaml.dart';
 
-/// A package internal representation of a pubspec file.
+/// A package internal representation of a `pubspec.yaml` file.
 ///
 /// <https://pub.dev/packages/pubspec_parse> is a better alternative for most.
-final class Pubspec {
+@immutable
+final class Pubspec extends YamlWrapper {
   /// Parses the pubspec from the given YAML document [contents].
   factory Pubspec.parseFrom(String contents, {Uri? sourceUrl}) {
     final doc = loadYamlDocument(contents, sourceUrl: sourceUrl);
@@ -27,68 +29,26 @@ final class Pubspec {
     return Pubspec._(YamlMap.wrap(root));
   }
 
-  const Pubspec._(this._root);
-  final YamlMap _root;
-
-  String? _loadString(String key) {
-    final value = _root[key];
-    if (value == null) {
-      return null;
-    }
-    if (value is! String) {
-      throw FormatException(
-        'Expected a string, got ${value.runtimeType}',
-        _root.span.text,
-      );
-    }
-    return value;
-  }
-
-  List<String>? _loadStringList(String key) {
-    final value = _root[key];
-    if (value == null) {
-      return null;
-    }
-    if (value is! List) {
-      throw FormatException(
-        'Expected a list, got ${value.runtimeType}',
-        _root.span.text,
-      );
-    }
-    if (value.any((e) => e is! String)) {
-      throw FormatException(
-        'Expected a list of strings',
-        _root.span.text,
-      );
-    }
-    return List<String>.from(value);
-  }
-
-  Never _isRequired(String name) {
-    throw FormatException(
-      'Missing required field: $name',
-      _root.span.text,
-    );
-  }
+  const Pubspec._(super.root);
 
   /// Name of the package.
-  String get name => _loadString('name') ?? _isRequired('name');
+  String get name => loadString('name') ?? isRequired('name');
 
   /// If publishing is enabled.
-  bool get isPublishable => _root['publish'] != 'none';
+  bool get isPublishable => root['publish'] != 'none';
 
   /// Description of the package.
   ///
   /// If omitted, the default is `null`.
-  String? get description => _loadString('description');
+  String? get description => loadString('description');
 
   /// Short description of the package.
   ///
   /// If omitted, the default is `null`.
-  String? get shortDescription => _loadString('short_description');
+  String? get shortDescription => loadString('short_description');
 
   /// Packages in the workspace.
   ///
   /// If this is not a workspace, the default is `null`.
-  List<String>? get workspace => _loadStringList('workspace');
+  List<String>? get workspace => loadStringList('workspace');
 }
---
+++ packages/chore/lib/src/internal/yaml_wrapper.dart
@@ -0,0 +1,77 @@
+@internal
+library;
+
+import 'package:meta/meta.dart';
+import 'package:yaml/yaml.dart';
+
+/// A package internal representation of a YAML file.
+@immutable
+base class YamlWrapper {
+  /// Creates a new YAML wrapper around the given [root] node.
+  const YamlWrapper(this.root);
+
+  /// Root node of the YAML document.
+  @protected
+  final YamlMap root;
+
+  /// Loads a string from the YAML document.
+  ///
+  /// If the key is missing, returns `null`.
+  ///
+  /// If the value is not a string, throws a [FormatException].
+  @protected
+  String? loadString(String key) {
+    final value = root[key];
+    if (value == null) {
+      return null;
+    }
+    if (value is! String) {
+      throw FormatException(
+        'Expected a string, got ${value.runtimeType}',
+        root.span.text,
+      );
+    }
+    return value;
+  }
+
+  /// Loads a list of strings from the YAML document.
+  ///
+  /// If the key is missing, returns `null`.
+  ///
+  /// If the value is not a list, throws a [FormatException].
+  @protected
+  List<String>? loadStringList(String key) {
+    final value = root[key];
+    if (value == null) {
+      return null;
+    }
+    if (value is! List) {
+      throw FormatException(
+        'Expected a list, got ${value.runtimeType}',
+        root.span.text,
+      );
+    }
+    if (value.any((e) => e is! String)) {
+      throw FormatException(
+        'Expected a list of strings',
+        root.span.text,
+      );
+    }
+    return List<String>.from(value);
+  }
+
+  /// Throws a standard [FormatException] for a missing required field.
+  ///
+  /// ## Example
+  ///
+  /// ```dart
+  /// String get name => loadString('name') ?? isRequired('name');
+  /// ```
+  @protected
+  Never isRequired(String name) {
+    throw FormatException(
+      'Missing required field: $name',
+      root.span.text,
+    );
+  }
+}
