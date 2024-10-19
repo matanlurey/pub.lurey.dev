2773fc72902cf9605b458c477fd4c29144c25191
Matan Lurey <matan@lurey.org>
2024-10-18 21:50:50 -0700

    Remove unused generator.

--- dev/lib/src/commands/generate.dart
+++ dev/lib/src/commands/generate.dart
@@ -3,7 +3,6 @@ import 'dart:io' as io;
 
 import 'package:chore/chore.dart';
 import 'package:dev/src/generators/changelog_header.dart';
-import 'package:dev/src/generators/codecov_yaml.dart';
 import 'package:dev/src/generators/github_package_workflow.dart';
 import 'package:dev/src/generators/package_readme.dart';
 import 'package:dev/src/generators/root_readme.dart';
@@ -82,8 +81,5 @@ final class Generate extends BaseCommand {
     await sink.writeRegions('README.md', {
       'PACKAGE_TABLE': generateRootReadmeRegion(packages),
     });
-
-    // Generate codecov.yaml.
-    await sink.write('codecov.yaml', generateCodecovYaml(packages: packages));
   }
 }
